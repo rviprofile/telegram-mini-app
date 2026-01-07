80b4a431d86f931e7539731e38380111161c5f1f
rviprofile <rvi.profile@gmail.com>
2026-01-07 17:14:45 +0500

    show info from tg

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -2,6 +2,18 @@ import { Box, Stack, Text } from "@chakra-ui/react";
 import { NavMenu } from "../components/ui/NavMenu/NavMenu";
 import { useTelegram } from "../hooks/useTelegram";
 
+// user: import('valibot').OptionalSchema<import('./pipes.js').JsonToSchemaPipe<import('valibot').LooseObjectSchema<{
+//             added_to_attachment_menu: import('valibot').OptionalSchema<import('valibot').BooleanSchema<undefined>, undefined>;
+//             allows_write_to_pm: import('valibot').OptionalSchema<import('valibot').BooleanSchema<undefined>, undefined>;
+//             first_name: StringSchema<undefined>;
+//             id: NumberSchema<undefined>;
+//             is_bot: import('valibot').OptionalSchema<import('valibot').BooleanSchema<undefined>, undefined>;
+//             is_premium: import('valibot').OptionalSchema<import('valibot').BooleanSchema<undefined>, undefined>;
+//             last_name: import('valibot').OptionalSchema<StringSchema<undefined>, undefined>;
+//             language_code: import('valibot').OptionalSchema<StringSchema<undefined>, undefined>;
+//             photo_url: import('valibot').OptionalSchema<StringSchema<undefined>, undefined>;
+//             username: import('valibot').OptionalSchema<StringSchema<undefined>, undefined>;
+
 export const Profile = () => {
   const tg = useTelegram();
 
@@ -14,19 +26,19 @@ export const Profile = () => {
       ) : (
         <Box>
           <Text>
-            <b>ID:</b> {tg.initDataUnsafe?.user?.id}
+            <b>ID:</b> {tg.tgWebAppData.user.id}
           </Text>
           <Text>
-            <b>Имя:</b> {tg.initDataUnsafe?.user?.first_name || "-"}
+            <b>Имя:</b> {tg.tgWebAppData.user.first_name}
           </Text>
           <Text>
-            <b>Фамилия:</b> {tg.initDataUnsafe?.user?.last_name || "-"}
+            <b>Фамилия:</b> {tg.tgWebAppData.user.last_name}
           </Text>
           <Text>
-            <b>Username:</b> {tg.initDataUnsafe?.user?.username || "-"}
+            <b>Username:</b> {tg.tgWebAppData.user.username}
           </Text>
           <Text>
-            <b>Язык:</b> {tg.initDataUnsafe?.user?.language_code || "-"}
+            <b>Язык:</b> {tg.tgWebAppData.user.language_code}
           </Text>
         </Box>
       )}
