6fd6e5eafb56b0090466b07f6efb27ceeda5d339
rviprofile <rvi.profile@gmail.com>
2026-02-01 19:16:32 +0500

    show initdata

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -32,7 +32,7 @@ export const Profile = () => {
             <b>Язык:</b> {tg.tgWebAppData.user.language_code}
           </Text>
           <Text>
-            <b>initData:</b> {tg.initData ? "Существует" : "Не найдено"}
+            <b>initData:</b> {tg.initData ? "Существует" : tg.initData}
           </Text>
           <Text>
             <b>Токены:</b> {auth.tokens ? "Получены" : "Не получены"}
